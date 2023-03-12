e2a42b8d0fbd4622caa329e78ca96a4679753eb6
Igor Melo <igoracmelo@protonmail.com>
2023-03-12 02:15:51 -0300

    bench: improve for default and add to `--fast`

--- main_test.go
+++ main_test.go
@@ -1,14 +1,38 @@
 package main
 
 import (
-	"os"
+	"os/exec"
 	"testing"
 )
 
-func Benchmark_main(b *testing.B) {
-	os.Args = []string{"dups", "./samples"}
+func BenchmarkDups_Defaults(b *testing.B) {
+	err := exec.Command("go", "build", "-o", "dups", ".").Run()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		main()
+		err = exec.Command("./dups", "./samples").Run()
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkDups_Fast(b *testing.B) {
+	err := exec.Command("go", "build", "-o", "dups", ".").Run()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		err = exec.Command("./dups", "--fast", "./samples").Run()
+		if err != nil {
+			b.Error(err)
+		}
 	}
 }
